ef303b8780901103de0930bfa60e70af2528b304
Krishna <krishna13248@gmail.com>
2022-02-02 20:18:23 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styling/App.css";
+import C1 from "./Components/card1";
+import C2 from "./Components/card2";
+import C3 from "./Components/card3";
+import C4 from "./Components/card4";
+
+function App() {
+  return (
+    <div className="digital-business-cards">
+      <C1 />
+      <C2 />
+      <C3 />
+      <C4 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/card3.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Image from "../Images/about.jpg";
+
+function Card() {
+  return (
+    <div className="card__3">
+      <img src={Image} alt="profile" className="card__profile" />
+
+      <div className="c3__myself flex">
+        <h2 className="c3__Name">Laura Smith</h2>
+        <h4>Frontend Developer</h4>
+        <p>
+          <a href="##">laurasmith.website</a>
+        </p>
+
+        <div className="c3__buttons">
+          <a href="##" className="white__button">
+            <i class="bx bxl-gmail"></i> <span>Email</span>
+          </a>
+          <a href="##" className="blue__button">
+            <i class="bx bxl-linkedin-square"></i> <span>LinkedIn</span>
+          </a>
+        </div>
+
+        <div className="about">
+          <div className="about__section">
+            <h2>About</h2>
+            <p>
+              I am a frontend developer with a particular interest in making
+              things simple and automating daily tasks. I try to keep up with
+              security and best practices, and am always looking for new things
+              to learn.
+            </p>
+          </div>
+
+          <div className="interest__section">
+            <h2>Interest</h2>
+            <p>
+              Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+              Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer flex">
+        <div className="footer-icons">
+          <i class="bx bxl-twitter"></i>
+          <i class="bx bxl-facebook"></i>
+          <i class="bx bxl-instagram"></i>
+          <i class="bx bxl-github"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function C3() {
+  return (
+    <div className="card light-theme">
+      <Card />
+    </div>
+  );
+}
+
+export default C3;
